b08b9dd1bf32733ac30b79c23451078fa92a0442
Aritra Bera <aritrabera69@gmail.com>
2024-05-12 13:38:04 +0530

    add timestamp

--- index.js
+++ index.js
@@ -1,40 +1,29 @@
-const express = require('express')
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const { Server } = require("socket.io");
 
 const app = express();
-
-const http = require('http');
-
-const colors = require('colors')
-
-const path = require('path')
-
-const { Server } = require("socket.io");
-const { Socket } = require('engine.io');
-
 const server = http.createServer(app);
-
-const io = new Server(server)
-
+const io = new Server(server);
 const PORT = 8080;
 
-app.use(express.static(path.resolve("./public")))
+app.use(express.static(path.resolve(__dirname, "./public")));
 
-//Socket.io
+// Socket.io
 io.on('connection', (client) => {
-    console.log('a new user connected', client.id);
+    console.log('A new user connected', client.id);
 
     client.on("user-message", (message) => {
-        io.emit('message', message)
-    })
+        const timestamp = new Date().toLocaleTimeString();
+        io.emit('message', `[${timestamp}] ${message}`);
+    });
 
     client.on('disconnect', () => {
-        console.log('user disconnected');
+        console.log('User disconnected');
     });
 });
 
-
-app.get('/', (req, res) => {
-    return res.sendFile("/public/index.html")
-})
-
-server.listen(PORT, () => { console.log(`Server startred at PORT: ${PORT}`.bgBlue.white) })
+server.listen(PORT, () => {
+    console.log(`Server started at PORT: ${PORT}`);
+});
